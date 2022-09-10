a7492825e33e8012ece3a3d0ab6edbdedcfbaf71
eslamnader <eslamnader@outlook.com>
2022-09-10 15:27:30 +0200

    Setting and DashBord

--- image-filter 092022/src/server.ts
+++ image-filter 092022/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
-
 (async () => {
 
   // Init the Express application
@@ -28,6 +27,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  app.get('/filteredimage', async (req, res) => {
+    const { image_url } = req.query;
+    console.log(image_url);
+    if (!image_url) res.status(404).json({ error: "Image link not found" })
+    await filterImageFromURL(image_url).then(async (response) => {
+      await res.status(200).sendFile(response);
+      setTimeout(() => {
+        deleteLocalFiles([response]);
+      }, 10000);
+    }).catch((error) => res.status(500).json({
+      error
+    }));
+  });
 
   //! END @TODO1
   
